ba8f34031e417c175820c8cd888065b8586c36d3
Kenneth <alcanamay@gmail.com>
2023-10-27 19:13:56 -0600

    Practica 6

---
+++ JS/script6.js
@@ -0,0 +1,34 @@
+async function obtenerDatosDelJSON() {
+    const $materias = document.getElementById("materias");
+
+let html = "";
+
+try {
+    let res = await fetch("js/mat.json");
+
+
+    if (!res.ok) {
+    throw "Error al acceder al archivo JSON";
+    }
+
+    let json = await res.json();
+
+
+    json.materias.forEach((el) => {
+    html += `
+        <div >
+        <h4>${el.nombre}</h4>
+        <h5>${el.docente}</h5>
+        <p>${el.desc}</p>
+        <br></br>
+        </div>
+    `;
+    });
+
+    $materias.innerHTML = html;
+} catch (error) {
+    console.warn(error);
+}
+}
+
+obtenerDatosDelJSON();
